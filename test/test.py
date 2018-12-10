62388f3a994691c4884766f7519951e5d67bf56c
neolin <lnonly@163.com>
2018-12-10 13:45:03 +0800

    MNT: add local repository v1.1 done

--- test/test.py
+++ test/test.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     t = TimerPlus()
 
     job_container_='csv'
-    filepath=r'E:\timerplus\test\joblist.csv'
+    filepath=r'E:\jt-timerplus\test\joblist.csv'
     t.add_jobs(job_container_=job_container_, filepath=filepath)
 
 
